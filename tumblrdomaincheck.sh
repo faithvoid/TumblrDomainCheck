bf3839fde6f3da584b4b636ae0474966df21a693
Lavender Starlight <56975081+faithvoid@users.noreply.github.com>
2023-07-21 17:20:16 -0400

    Update tumblrdomaincheck.sh

--- tumblrdomaincheck.sh
+++ tumblrdomaincheck.sh
@@ -6,19 +6,28 @@ function is_tumblr_domain_available() {
     local page_content=$(curl -s "$url")
 
     if [[ "$page_content" =~ "There's nothing here." ]]; then
-        echo "The domain '$domain' is available!"
+        return 0 # Domain available
     else
-        echo "The domain '$domain' is in use."
+        return 1 # Domain in use
     fi
 }
 
 # Replace the array elements with the Tumblr domains you want to monitor
 tumblr_domains=("example1.tumblr.com" "example2.tumblr.com" "example3.tumblr.com")
 
+function show_notification() {
+    local domain="$1"
+    notify-send "Tumblr Domain Available" "The domain '$domain' is now available!"
+}
+
 while true; do
     for tumblr_domain in "${tumblr_domains[@]}"; do
-        status=$(is_tumblr_domain_available "$tumblr_domain")
-        echo "$status"
+        if is_tumblr_domain_available "$tumblr_domain"; then
+            echo "The domain '$tumblr_domain' is available!"
+            show_notification "$tumblr_domain" # Show notification when domain is available
+        else
+            echo "The domain '$tumblr_domain' is in use."
+        fi
     done
 
     sleep 3600 # Wait for an hour (3600 seconds) before checking again
